83ff775218d4038d99c5a473582545b844aec5d9
Gerasik <gerasik1992@gmail.com>
2017-10-19 20:22:53 +0300

    Update connect.php

--- lib/connect.php
+++ lib/connect.php
@@ -1,9 +1,9 @@
 <?
 
-	define ('HOST', 'localhost');
-	define ('USER', 'root');
-	define ('PASS', '');
-	define ('DB', 'photohost');	
+	define ('HOST', 'us-cdbr-iron-east-05.cleardb.net');
+	define ('USER', 'b6c4c7362a4a33');
+	define ('PASS', '621c84c9');
+	define ('DB', 'heroku_a666ff323487205');	
 
 @mysql_connect(HOST, USER, PASS)
 	or die ("Ошибка подключения к базе данных");
